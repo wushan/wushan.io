2f2be7ea862d5e64bd025c3294a43c76fd37a556
wushan <chiangloveslynn@gmail.com>
2017-01-05 11:24:57 +0800

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nuxt/>
+    <my-footer/>
+  </div>
+</template>
+
+<script>
+import MyFooter from '~components/Footer.vue'
+
+export default {
+  components: {
+    MyFooter
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: #f7f7f7;
+}
+.container {
+  padding: 2em;
+  box-sizing: border-box;
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  -webkit-align-content: center;
+          align-content: center;
+  -webkit-justify-content: center;
+          justify-content: center;
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  section
+    .container
+      img(src="~assets/img/wushanio-logo.svg")
+</template>
+<script>
+export default {
+  head: {
+    title: 'wushan.io - a Graphic designer & Frontend developer base in Taiwan.',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  }
+}
+</script>
+<style scoped>
+</style>
